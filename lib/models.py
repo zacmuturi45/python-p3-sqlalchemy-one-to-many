d6e422c0e3bc4e33fef5fa629b7f90b21a9f0ae0
isaacgichuhi <zacmuturi45@gmail.com>
2023-12-22 16:32:12 +0300

    Completed assignment

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, create_engine
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,4 +7,33 @@ convention = {
 }
 metadata = MetaData(naming_convention=convention)
 
+engine = create_engine('sqlite:///one_to_many.db')
 Base = declarative_base(metadata=metadata)
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+    reviews = relationship('Review', backref=backref('game'))
+
+    def __repr__(self):
+        return f'Game(id={self.id}, ' + \
+            f'title={self.title}, ' + \
+            f'platform={self.platform})'
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    score = Column(Integer())
+    comment = Column(String())
+    game_id = Column(Integer(), ForeignKey('games.id'))
+
+    def __repr__(self):
+        return f'Review(id={self.id}, ' + \
+            f'score={self.score}, ' + \
+            f'game_id={self.game_id})'
